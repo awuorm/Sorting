f60a09204a602ec29c0128644a96e57851bea768
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-02-17 18:41:21 +0300

    used while removed sort method

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -13,27 +13,40 @@ def selection_sort(arr):
             if len(rh_side) != 0:
                 small_value = min(rh_side)
                 arr.remove(small_value)
-                arr.insert(smallest_index,small_value)
+                arr.insert(smallest_index, small_value)
         print(arr)
         return arr
-selection_sort([25, 67, 4,33,19])
-selection_sort([33,19,150,123,167,190])
+
+
+selection_sort([25, 67, 4, 33, 19])
+selection_sort([33, 19, 150, 123, 167, 190])
 
 # TO-DO:  implement the Bubble Sort function below
+
+
 def bubble_sort(arr):
     sorted_arr = arr
-    if arr != sorted_arr.sort():
+    print(arr[0], min(arr), max(arr), arr[len(arr) - 1])
+    while arr[0] != min(arr) and arr[len(arr) - 1] != max(arr) or arr[0] == min(arr) and arr[len(arr) - 1] != max(arr) or arr[0] != min(arr) and arr[len(arr) - 1] == max(arr):
         for i in range(0, len(arr) - 1):
-           cur_index = i
-           smallest_index = cur_index
-           next_index = cur_index + 1
-           if arr[smallest_index] >= arr[next_index]:
+            cur_index = i
+            smallest_index = cur_index
+            next_index = cur_index + 1
+            if arr[smallest_index] >= arr[next_index]:
                 add_item = arr[next_index]
                 arr.remove(arr[next_index])
-                arr.insert(smallest_index,add_item)
-        print(arr)
-        return arr
-bubble_sort([25, 67, 4,33,19,40,190,200,1])
+                arr.insert(smallest_index, add_item)
+                print(arr)
+            else:
+                if arr[smallest_index] <= arr[next_index]:
+                    add_item = arr[smallest_index]
+                    arr.remove(arr[smallest_index])
+                    arr.insert(smallest_index, add_item)
+    # print(arr)
+    return arr
+
+
+bubble_sort([25, 67, 4, 33, 19, 40, 190, 200, 1])
 
 
 # STRETCH: implement the Count Sort function below
